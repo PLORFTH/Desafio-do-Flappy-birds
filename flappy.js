35fb02c628d9f450e3614ac168a0f897d89349f6
PLORFTH <victorleriano@gmail.com>
2023-01-07 16:16:01 -0300

    criando uma função para sortear a altura da Barreira

--- flappy.js
+++ flappy.js
@@ -6,6 +6,10 @@ function novoElemento(tagName, className) {
 //Essa função ja cria uma tag com a classe que a gente atribuir a ela
 //ex: <div class="conteudo"></div> só que a função ja faz isso automatico
 
+
+//o this é para podermos utilizar o atributo criado numa função, globalmente
+//e atribuir novos atributos a ele e ele vai herdando esses atributos como um objeto
+
 function Barreira(reversa = false){
     this.elemento = novoElemento('div', 'barreira');
 
@@ -17,6 +21,34 @@ function Barreira(reversa = false){
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
 
-const b = new Barreira(true)
-b.setAltura(500)
+// const b = new Barreira(true) essas 3 linhas é para testar se funciona o codigo
+// b.setAltura(400)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.getY = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+const b = new ParDeBarreiras(700, 200, 400)
 document.querySelector('[wm-flappy]').appendChild(b.elemento)
